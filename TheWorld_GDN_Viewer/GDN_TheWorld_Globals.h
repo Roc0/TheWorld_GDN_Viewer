06e934f6059049c5df79cda744c0fb189dec9f80
Riccardo Marini <riccardo.marini65@live.com>
2022-01-09 10:52:09 +0100

    wip

--- TheWorld_GDN_Viewer/GDN_TheWorld_Globals.h
+++ TheWorld_GDN_Viewer/GDN_TheWorld_Globals.h
@@ -35,7 +35,7 @@ namespace godot
 			register_method("get_lod_max_depth", &GDN_TheWorld_Globals::lodMaxDepth);
 			register_method("get_num_lods", &GDN_TheWorld_Globals::numLods);
 			register_method("get_chunks_per_bitmap_side", &GDN_TheWorld_Globals::numChunksPerBitmapSide);
-			register_method("get_grid_step_in_wu", &GDN_TheWorld_Globals::gridStepInBitmapWU);
+			register_method("get_grid_step_in_wu", &GDN_TheWorld_Globals::gridStepInBitmapWUs);
 		}
 
 		//
@@ -48,16 +48,29 @@ namespace godot
 
 		TheWorld_MapManager::MapManager* mapManager(void) { return m_mapManager; };
 
-		// The world is rapresented by one or more mesh : 1 mesh for every chunk which contains the grid of vertices whose elevations are all contained in a bitmap.
-		// The elevations are contained in a bitmap (for shader purposes) whose side has a number of vertices (bitmapResolution) which is multiple of the number of vertices of every chunk (numVerticesPerChuckSide); all th vertices
-		//		of the bitmpa relate to a quad tree of chunks and as the meshes don't know about their positions in global coordinate system
-		// The chunks are organized in quads which means that every chunk can be split in 4 chunk (uppepr-left, upper-right, lesser-left, lesser-right) at every split the lod value is decremented by 1 until lod value = 0 at which
-		//		the chunk has the most defined mesh while at lod = numLods - 1 has the less defined mesh
-		//		Every chunk has a fixed number of vertices for every side at every lod (numVerticesPerChuckSide), at higher lods the vertices are more spaced, at lod 0 the vertices are at the maximum density
-		//		The lod value is an index whose range is from 0 (the maximum level of details in which the vertex are less spaced and the details are more defined at his level the highest number of chunk is required to fullfill
-		//		the elevation bitmap) to max depth (maxDepth) = number of lods (numLods) - 1 (the maximum value of the index which matches the less defined mesh in which the vertices are more spaced and only 1 chunk is required
-		//		to cover the elevation bitmap)
-		//		The meshes of the chunks are expressed in local coord and the chunk is responsible to position them in he real world know their positions in global coordinate system
+		// WORLD GRID
+		// The world is rapresented by a grid map (World Grid) streamed from external source which defines also the distance of each vertex of the grid map in WUs. Such a grid is a flat grid of vertices whose elevations
+		//		are contained in a bitmap.
+		// 
+		// THE BITMAP: ELEVATIONS
+		// The elevations are contained in a bitmap (for shader purposes) each for every vertex of the World Grid. The side of the bitmap has a number of vertices (bitmapResolution) which is multiple of the number
+		//		of vertices of every chunk (numVerticesPerChuckSide); all th vertices of the bitmap relate to a quad tree of chunks and such vertices don't know about their positions in global coordinate system but
+		//		only to their relative position in the chunk: to compute it, it is necessary to know the chunk position in global coords and its lod (which expresses how the vertices are spaced in number of World Grid vertices).
+		//
+		// CHUNKS AND QUADS
+		// The world is subdivided in chunks by one or more mesh : 1 mesh for every chunk which contains the grid of vertices whose elevations are all contained in a bitmap (mostly in a part of it only at lower resolution
+		//		there is only a chunk in which tge vertices are more spaced).
+		// The world is subdivided in chunks organized in quads which means that every chunk can be split in 4 chunk (upper-left, upper-right, lesser-left, lesser-right) at every split the lod value is decremented by 1
+		//		until lod value = 0 at which the chunk has the most defined mesh while at lod = numLods - 1 has the less defined mesh.
+		//		Every chunks contain a mesh which contains the grid of vertices whose elevations are all contained in a bitmap (mostly in a part of it only at lower resolution there is only a chunk in which the vertices
+		//		are more spaced).
+		//		Every chunk has a fixed number of vertices for every side at every lod (numVerticesPerChuckSide), at higher lods the vertices are more spaced (in terms of World Grid vertices), at lod 0 the vertices are at
+		//		the maximum density. The lod value is an index whose range is from 0 (the maximum level of details in which the vertex are less spaced and the details are more defined at his level the highest number of
+		//		chunk is required to fullfill the elevation bitmap and the granularity of the vertices is the same of the World Grid) to max depth (maxDepth) = number of lods (numLods) - 1 (the maximum value of the index
+		//		which matches the less defined mesh in which the vertices are more spaced and only 1 chunk is required to cover the elevation bitmap).
+		//		The meshes of the chunks are expressed in local coord and the chunk is responsible to position them in he real world knowing their positions in global coordinate system.
+		//
+		// IN SHORT
 		// In short: meshes and bitmap are expressed in local coordinate system, the bitmap is related to a quad tree of chunks and the chunks know the position aof every mesh in global coordinate system as the shaders know
 		//		the elvation of every vertex in the bitmap (???)
 		// At maximum lod (numLods - 1) the world is rapresented by a single chunk/mesh which can be split until lod = 0 for the maximun number of chunks, the point is that not every chunk of the world rapresentaion are at the
@@ -65,16 +78,16 @@ namespace godot
 		// The maximum lod value (lodMaxDepth) is the maximum number of split - 1 that can be done so that all the vertices of all the chunks can be contained in the bitmap : it is computed as the power of 2 of the maximum number
 		//		of chunks of a side of the bitmap (bitmapResolution / numVerticesPerChuckSide), the number of lods (numLods) is equal to lodMaxDepth + 1
 
-		// Number of vertices of the side of a chunk (-1) which is fixed at every lod and is a multiple of 2
+		// Number of vertices of the side of a chunk (-1) which is fixed (not a function of the lod) and is a multiple of 2
 		int numVerticesPerChuckSide(void) { return m_numVerticesPerChuckSide; }					// Chunk num vertices -1
 		
 		// Number of vertices of the side of the bitmap (-1) with the elevations which is fixed and is a multiple of the number of vertices of the side of a chunk (numVerticesPerChuckSide) and is for this a multiple of 2 too
 		int bitmapResolution(void) { return m_bitmapResolution; }	// Resolution of the bitmap = num point of the bitmap -1;
 
-		// Max number of quad split that can be done so that the bitmap can contain all the vertices
+		// Max value of the lod index (numLods - 1)
 		int lodMaxDepth(void) {	return m_lodMaxDepth; }
 
-		// Max number of level of detail possible so that the bitmap can contain all the vertices
+		// Max number of quad split that can be done until the the granularity of the vertices is the same of the World Grid map and of the bitmap (lodMaxDepth + 1)
 		int numLods(void) { return m_numLods; }
 
 		// The number of chunks required to cover every side of the bitmap at the specified lod value
@@ -86,8 +99,8 @@ namespace godot
 			return (bitmapResolution() / numVerticesPerChuckSide()) >> lod;
 		}
 		
-		// The number of grid vertices (-1) which separate the vertices of the mesh at the specified lod value
-		int strideInGrid(int lod) { return gridStepInBitmap(lod); }
+		// The number of World Grid vertices (-1) or bitmap vertices (-1) which separate the vertices of the mesh at the specified lod value
+		int strideInWorldGrid(int lod) { return gridStepInBitmap(lod); }
 		int gridStepInBitmap(int lod)
 		{
 			if (lod < 0 || lod > lodMaxDepth())
@@ -99,7 +112,8 @@ namespace godot
 		}
 
 		// The number of World Units which separate the vertices of the mesh at the specified lod value
-		float gridStepInBitmapWU(int lod)
+		float strideInWorldGridWUs(int lod) { return gridStepInBitmapWUs(lod); }
+		float gridStepInBitmapWUs(int lod)
 		{
 			assert(!(lod < 0 || lod > lodMaxDepth()));
 			if (lod < 0 || lod > lodMaxDepth())
--- TheWorld_GDN_Viewer/MeshCache.cpp
+++ TheWorld_GDN_Viewer/MeshCache.cpp
@@ -24,36 +24,37 @@ namespace godot
 
 		m_meshCache.resize(SEAM_CONFIG_COUNT);
 		
-		for (int idxSeams = 0; idxSeams < SEAM_CONFIG_COUNT; idxSeams++)
+		for (int seams = 0; seams < SEAM_CONFIG_COUNT; seams++)
 		{
-			m_meshCache[idxSeams].resize(m_viewer->Globals()->numLods());
+			m_meshCache[seams].resize(m_viewer->Globals()->numLods());
 
 			for (int idxLod = 0; idxLod < m_viewer->Globals()->numLods(); idxLod++)
 			{
-				m_meshCache[idxSeams][idxLod] = makeFlatChunk(numVerticesPerChuckSide, idxLod, idxSeams);
+				m_meshCache[seams][idxLod] = makeFlatChunk(numVerticesPerChuckSide, idxLod, seams);
 			}
 		}
 	}
 
 	void MeshCache::resetCache(void)
 	{
-		for (int idxSeams = 0; idxSeams < m_meshCache.size(); idxSeams++)
+		for (int seams = 0; seams < m_meshCache.size(); seams++)
 		{
-			for (int idxLod = 0; idxLod < m_meshCache[idxSeams].size(); idxLod++)
+			for (int idxLod = 0; idxLod < m_meshCache[seams].size(); idxLod++)
 			{
-				m_meshCache[idxSeams][idxLod]->call_deferred("free");
+				m_meshCache[seams][idxLod]->call_deferred("free");
 			}
-			m_meshCache[idxSeams].clear();
+			m_meshCache[seams].clear();
 		}
 		m_meshCache.clear();
 	}
 
-	ArrayMesh* MeshCache::makeFlatChunk(int numVerticesPerChuckSide, int lod, int seams)
+	ArrayMesh* MeshCache::makeFlatChunk(int numVerticesPerChuckSide, int idxLod, int seams)
 	{
 		//	https://gist.github.com/bnolan/01a7d240774f1e02056d6607e5b621da
 
-		int stride = m_viewer->Globals()->strideInGrid(lod);
+		int stride = m_viewer->Globals()->strideInWorldGrid(idxLod);
 
+		// Determines the vertices in a flat mesh at the required lod using Worl Grid vertices
 		PoolVector3Array positions;
 		positions.resize((int)pow(numVerticesPerChuckSide + 1, 2));
 		for (real_t z = 0; z < numVerticesPerChuckSide + 1; z++)
@@ -61,6 +62,7 @@ namespace godot
 				positions.append(Vector3(x * stride, 0, z * stride));
 		
 		PoolIntArray indices;
+		// Preparing array of indices releate to vertex array to form the triangles of the mesh: a tringle every three indices
 		void makeFlatChunk_makeIndices(PoolIntArray& indices, int numVerticesPerChuckSide, int seams);
 		makeFlatChunk_makeIndices(indices, numVerticesPerChuckSide, seams);
 
@@ -78,6 +80,280 @@ namespace godot
 
 void makeFlatChunk_makeIndices(godot::PoolIntArray& indices, int numVerticesPerChuckSide, int seams)
 {
-	// TODO
+	// only even sizes admitted
+	assert(numVerticesPerChuckSide % 2 == 0);
+	
+	int firstColumnOfRegularTriangles = 0;
+	int firstRowOfRegularTriangles = 0;
+	int numberOfColumnsOfRegularTriangles = numVerticesPerChuckSide;	// num vertices (-1) on each row
+	int numberOfRowsOfRegularTriangles = numVerticesPerChuckSide;		// num rows (-1)
+	int deviationToFirstVertxeOfNextRow = 1;							// To point the first vertex on next row when current vertex is the last of the row
+
+	if (seams & SEAM_LEFT)
+	{
+		// Seam on the left: need to adjust to point all the vertices not on the border subject to a seam
+		firstColumnOfRegularTriangles += 1;		// bypassing first vertex on every row
+		numberOfColumnsOfRegularTriangles -= 1;	// as we bypass first vertex the number of the columns (the size of the row) is decremented by 1
+		deviationToFirstVertxeOfNextRow += 1;	// and we increment by 1 the deviation to bypass the first vertex on rows next to the first
+	}
+
+	if (seams & SEAM_BOTTOM)
+	{
+		// Seam on the bottom: need to adjust to point all the vertices not on the border subject to a seam
+		firstRowOfRegularTriangles += 1;		// bypassing first row
+		numberOfRowsOfRegularTriangles -= 1;	// and of course decrementing the number of the rows
+	}
+
+	if (seams & SEAM_RIGHT)
+	{
+		// Seam on the rigth: need to adjust to point all the vertices not on the border subject to a seam
+		numberOfColumnsOfRegularTriangles -= 1;	// bypassing last vertex on every row decrementeing by 1 the number of the columns (the size of the row)
+		deviationToFirstVertxeOfNextRow += 1;	// and we increment by 1 the deviation to bypass the last vertex of every row
+	}
+
+	if (seams & SEAM_TOP)
+	{
+		// Seam on the top: need to adjust to point all the vertices not on the border subject to a seam
+		numberOfRowsOfRegularTriangles -= 1;	// decrementing number of rows by 1 to bypass last row
+	}
+
+	// inserting regular triangles = triangles not on the border subject to a seam (left, right, top, bottom)
+	int vertexIndex = firstColumnOfRegularTriangles + firstRowOfRegularTriangles * (numVerticesPerChuckSide + 1);
+	for (int z = 0; z < numberOfRowsOfRegularTriangles; z++)
+	{
+		// sliding all rows with the exclusion of the ones on the border if it nead a seam (on the top or on the bottom) against a less defined mesh
+		for (int x = 0; x < numberOfColumnsOfRegularTriangles; x++)
+		{
+			// sliding all vertex of a row with the exclusion of the ones on the border subject to a seam (on the left or on the right) against a less defined mesh
+			
+			//    x ==>
+			// z  00---10
+			// |   |\  |
+			// -   | \ |
+			//     |  \|
+			//    01---11
+
+			int ixz00 = vertexIndex;											// current vertex
+			int ixz10 = vertexIndex + 1;										// next vertex
+			int ixz01 = vertexIndex + numberOfColumnsOfRegularTriangles + 1;	// same vertex on next row
+			int ixz11 = ixz01 + 1;												// next vertex
+
+			// first triangle
+			indices.append(ixz00);
+			indices.append(ixz11);
+			indices.append(ixz01);
+
+			// second triangle
+			indices.append(ixz00);
+			indices.append(ixz10);
+			indices.append(ixz11);
+
+			// ponting next vertex on the row
+			vertexIndex += 1;
+		}
+		// ponting first vertex on next row: bypassing the border subject to a seam (on the left and on the right) against a less defined mesh
+		vertexIndex += deviationToFirstVertxeOfNextRow;
+	}
+	
+	// inserting triangles on the border subject to a seam (left, right, top, bottom)
+
+	if (seams & SEAM_LEFT)
+	{
+		//		x ==>
+		//	z	0 . 1
+		//	|	|\  .
+		//	-	| \ .
+		//		|  \.
+		//		(2)|   3
+		//		|  /.
+		//		| / .
+		//		|/  .
+		//		4 . 5
+
+		vertexIndex = 0;	// positioning on first vertex of first seam on the lower, left corner of the mesh (0): first vertex of first row
+		int numberOfSeamsOnTheLeft = numVerticesPerChuckSide / 2;
+
+		for (int idxSeam = 0; idxSeam < numberOfSeamsOnTheLeft; idxSeam++)
+		{
+			int i0 = vertexIndex;											// current vertex
+			int i1 = vertexIndex + 1;										// next vertex on same row
+			int i3 = vertexIndex + numVerticesPerChuckSide + 2;				// next vertex on next row
+			int i4 = vertexIndex + (numVerticesPerChuckSide + 1) * 2;		// same vertex 2 row below
+			int i5 = i4 + 1;												// next vertex 2 row below
+
+			// triangle against left chunk with higher lod index (one quad split lesser)
+			indices.append(i0);
+			indices.append(i3);
+			indices.append(i4);
+
+			// if not on first seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the first seam on the left and we dont have a seam on the bottom we need this triangle too to fill the hole (otherwise the bottom seam will fill the hole)
+			if (idxSeam != 0 || !(seams & SEAM_BOTTOM))
+			{
+				indices.append(i0);
+				indices.append(i1);
+				indices.append(i3);
+			}
+
+			// if not on last seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the last seam on the left and we dont have a seam on the top we need this triangle too to fill the hole (otherwise the top seam will fill the hole)
+			if (idxSeam != numberOfSeamsOnTheLeft - 1 || !(seams & SEAM_TOP))
+			{
+				indices.append(i3);
+				indices.append(i5);
+				indices.append(i4);
+			}
+
+			vertexIndex = i4;	// positioning on first vertex of next seam going uppward
+		}
+	}
+
+	if (seams & SEAM_RIGHT)
+	{
+		//		x ==>
+		//	z	0 . 1
+		//	|	.  /|
+		//	-	. / |
+		//		./  |
+		//		2 | (3)
+		//		.\  |
+		//		. \ |
+		//		.  \|
+		//		4 . 5
+
+		vertexIndex = numVerticesPerChuckSide - 1;	// positioning on first vertex of first seam on the lower, right corner of the mesh (0): penultimate vertex of first row
+		int numberOfSeamsOnTheRight = numVerticesPerChuckSide / 2;
+
+		for (int idxSeam = 0; idxSeam < numberOfSeamsOnTheRight; idxSeam++)
+		{
+			int i0 = vertexIndex;											// current vertex
+			int i1 = vertexIndex + 1;										// next vertex on same row
+			int i2 = vertexIndex + numVerticesPerChuckSide + 1;				// same vertex on next row
+			int i4 = vertexIndex + (numVerticesPerChuckSide + 1) * 2;		// same vertex 2 row below
+			int i5 = i4 + 1;												// next vertex 2 row below
+
+			// triangle against left chunk with higher lod index (one quad split lesser)
+			indices.append(i1);
+			indices.append(i5);
+			indices.append(i2);
+
+			// if not on first seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the first seam on the right and we dont have a seam on the bottom we need this triangle too to fill the hole (otherwise the bottom seam will fill the hole)
+			if (idxSeam != 0 || !(seams & SEAM_BOTTOM))
+			{
+				indices.append(i0);
+				indices.append(i1);
+				indices.append(i2);
+			}
+
+			// if not on last seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the last seam on the right and we dont have a seam on the top we need this triangle too to fill the hole (otherwise the top seam will fill the hole)
+			if (idxSeam != numberOfSeamsOnTheRight - 1 || !(seams & SEAM_TOP))
+			{
+				indices.append(i2);
+				indices.append(i5);
+				indices.append(i4);
+			}
+
+			vertexIndex = i4;	// positioning on first vertex of next seam going uppward
+		}
+	}
+
+	if (seams & SEAM_BOTTOM)
+	{
+		//		x ==>
+		//		   (1)
+		//	z	0------ - 2
+		//	|	.\     /.
+		//	-	. \   / .
+		//		.  \ /  .
+		//		3 . 4 . 5
+
+		vertexIndex = 0;	// positioning on first vertex of first seam on the lower, left corner of the mesh (0): first vertex of first row
+		int numberOfSeamsOnTheBottom = numVerticesPerChuckSide / 2;
+
+		for (int idxSeam = 0; idxSeam < numberOfSeamsOnTheBottom; idxSeam++)
+		{
+			int i0 = vertexIndex;											// current vertex
+			int i2 = vertexIndex + 2;										// moving 2 vertex on the right on same row
+			int i3 = vertexIndex + numVerticesPerChuckSide + 1;				// same vertex on next row
+			int i4 = i3 + 1;												// next vertex on next row
+			int i5 = i4 + 1;												// moving 2 vertex on the right on next row
+
+			// triangle against left chunk with higher lod index (one quad split lesser)
+			indices.append(i0);
+			indices.append(i2);
+			indices.append(i4);
+
+			// if not on first seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the first seam on the left and we dont have a seam on the bottom we need this triangle too to fill the hole (otherwise the bottom seam will fill the hole)
+			if (idxSeam != 0 || !(seams & SEAM_LEFT))
+			{
+				indices.append(i0);
+				indices.append(i4);
+				indices.append(i3);
+			}
+
+			// if not on last seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the last seam on the left and we dont have a seam on the top we need this triangle too to fill the hole (otherwise the top seam will fill the hole)
+			if (idxSeam != numberOfSeamsOnTheBottom - 1 || !(seams & SEAM_RIGHT))
+			{
+				indices.append(i2);
+				indices.append(i5);
+				indices.append(i4);
+			}
+
+			vertexIndex = i2;	// positioning on first vertex of next seam going uppward
+		}
+	}
+
+	if (seams & SEAM_TOP)
+	{
+		//		x ==>
+		//	z	0 . 1 . 2
+		//	|	.  / \  .
+		//	-	. /   \ .
+		//		./     \.
+		//		3------ - 5
+		//		   (4)
+
+		vertexIndex = (numVerticesPerChuckSide - 1) * (numVerticesPerChuckSide + 1);	// positioning on first vertex of first seam on the upper, left corner of the mesh (0): first vertex of penultimate row
+		int numberOfSeamsOnTheTop = numVerticesPerChuckSide / 2;
+
+		for (int idxSeam = 0; idxSeam < numberOfSeamsOnTheTop; idxSeam++)
+		{
+			int i0 = vertexIndex;											// current vertex
+			int i1 = vertexIndex + 1;										// next vertex on same row
+			int i2 = vertexIndex + 2;										// moving 2 vertex on the right on same row
+			int i3 = vertexIndex + numVerticesPerChuckSide + 1;				// same vertex on next row
+			int i5 = i3 + 2;												// moving 2 vertex on the right on next row
+
+			// triangle against left chunk with higher lod index (one quad split lesser)
+			indices.append(i3);
+			indices.append(i1);
+			indices.append(i5);
+
+			// if not on first seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the first seam on the left and we dont have a seam on the bottom we need this triangle too to fill the hole (otherwise the bottom seam will fill the hole)
+			if (idxSeam != 0 || !(seams & SEAM_LEFT))
+			{
+				indices.append(i0);
+				indices.append(i1);
+				indices.append(i3);
+			}
+
+			// if not on last seam from bottom to top we need this triangle to fill the hole
+			// instead if we are on the last seam on the left and we dont have a seam on the top we need this triangle too to fill the hole (otherwise the top seam will fill the hole)
+			if (idxSeam != numberOfSeamsOnTheTop - 1 || !(seams & SEAM_RIGHT))
+			{
+				indices.append(i1);
+				indices.append(i2);
+				indices.append(i5);
+			}
+
+			vertexIndex = i2;	// positioning on first vertex of next seam going uppward
+		}
+	}
+
 	return;
 }
